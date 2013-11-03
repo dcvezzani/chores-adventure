a7b1b565ca286d688de1c6bcbb59a2535e557c37
David Vezzani <david_vezzani@yahoo.com>
2013-11-02 17:12:11 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chores::Application.config.secret_key_base = '7172d2f4cfbe32d8dc1625ecf7522c320100644d8f4f10bf95bb03ffc779ec22b93b70cb27275ace00c66dd4e445c70329bfa1f18c42d019c046e1d09c014d4e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Chores::Application.config.session_store :cookie_store, key: '_chores_session'
