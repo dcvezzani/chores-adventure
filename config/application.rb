eb6e6572a6210399f63d2eeb52ba17aa9cb8ed0b
David Vezzani <david_vezzani@yahoo.com>
2013-11-10 00:37:44 -0800

    adds static asset serving to config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,8 @@ module Chores
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.serve_static_assets = true
     
     config.assets.paths %W(#{config.root}/jquery-ui-1.10.3.custom/css/ui-lightness #{config.root}/custom #{config.root}/choco-templates/css #{config.root}/custom)
    
