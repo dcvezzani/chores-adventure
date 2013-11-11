a4788477899f843bf786788825982909a56ff8a9
David Vezzani <david_vezzani@yahoo.com>
2013-11-11 08:57:24 -0800

    pulls latest need additions to the slide show various costmetic adjustments to the reponsive layouts

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,6 @@ class WelcomeController < ApplicationController
   layout 'landing-page'
 
   def index
+    @latest_needs = Need.where{(picture != nil) & (picture != "")}.order{updated_at.desc}.limit(6)
   end
 end
