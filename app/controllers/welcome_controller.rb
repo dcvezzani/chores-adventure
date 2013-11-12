24d5c41768abb6d9456d710733b98c5731dcb9dd
David Vezzani <david_vezzani@yahoo.com>
2013-11-12 07:21:31 -0800

    protects from blank testimonial

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,6 @@ class WelcomeController < ApplicationController
 
   def index
     @latest_needs = Need.where{(picture != nil) & (picture != "")}.order{updated_at.desc}.limit(6)
-    @testimonial = Kudo.order{created_at.desc}.limit(1).first
+    @testimonial = Kudo.order{created_at.desc}.limit(1).first or Kudo.new(title: "Thank you!", description: "You see something nice?  Give some kudos!")
   end
 end
--- app/helpers/kudos_helper.rb
+++ app/helpers/kudos_helper.rb
@@ -1,2 +1,10 @@
 module KudosHelper
+  def say_thankyou(testimonial)
+    if(testimonial.recipient)
+  		@testimonial.title.capitalize + ", " + @testimonial.recipient.name
+    else
+  		@testimonial.title.capitalize
+    end
+  end
+
 end
