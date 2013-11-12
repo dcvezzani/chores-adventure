a2e2b53d1f5db0b80762637fbaaff1c6decf19fb
David Vezzani <david_vezzani@yahoo.com>
2013-11-12 07:44:31 -0800

    debug heroku view

--- app/helpers/kudos_helper.rb
+++ app/helpers/kudos_helper.rb
@@ -1,6 +1,7 @@
 module KudosHelper
   def say_thankyou(testimonial)
     if(testimonial.recipient)
+      Rails.logger.debug(">>> asdf")
   		@testimonial.title.capitalize + ", " + @testimonial.recipient.name
     else
   		@testimonial.title.capitalize
