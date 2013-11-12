dbf3d9587b43c38e0c4e7f5002e1d6fca62050ed
David Vezzani <david_vezzani@yahoo.com>
2013-11-12 08:27:16 -0800

    adds link to give kudos when there are not any current kudos available

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,6 @@ class WelcomeController < ApplicationController
 
   def index
     @latest_needs = Need.where{(picture != nil) & (picture != "")}.order{updated_at.desc}.limit(6)
-    @testimonial = (Kudo.count > 0) ? Kudo.order{created_at.desc}.limit(1).first : Kudo.new(title: "Thank you!", description: "You see something nice?  Give some kudos!")
+    @testimonial = Kudo.new(title: "Thank you!", description: "You see something nice?  <a href='/kudos/new'>Give some kudos</a>!")
   end
 end
--- app/helpers/kudos_helper.rb
+++ app/helpers/kudos_helper.rb
@@ -7,4 +7,15 @@ module KudosHelper
     end
   end
 
+  def testimonial_description(testimonial)
+    if(testimonial.description.blank?) 
+      "We sure appreciate your help!" 
+    else
+      if(testimonial.new_record?)
+        raw(testimonial.description)
+      else
+        testimonial.description
+      end
+    end
+  end
 end
