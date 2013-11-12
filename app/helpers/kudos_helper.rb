2bae116a33132b0cd92655f59d21a51439ad5d43
David Vezzani <david_vezzani@yahoo.com>
2013-11-12 07:48:09 -0800

    add debugging line

--- app/helpers/kudos_helper.rb
+++ app/helpers/kudos_helper.rb
@@ -1,9 +1,10 @@
 module KudosHelper
   def say_thankyou(testimonial)
     if(testimonial.recipient)
-      Rails.logger.debug(">>> asdf")
+      Rails.logger.debug(">>> asdf-001")
   		@testimonial.title.capitalize + ", " + @testimonial.recipient.name
     else
+      Rails.logger.debug(">>> asdf-002")
   		@testimonial.title.capitalize
     end
   end
