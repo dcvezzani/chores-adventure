55b60252c76e20154110d01be168190a1cf5f366
David Vezzani <david_vezzani@yahoo.com>
2013-11-12 07:54:42 -0800

    removes debugger lines; causing 500 on server? more explicit notation to create a default Kudo if none exists

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,6 @@ class WelcomeController < ApplicationController
 
   def index
     @latest_needs = Need.where{(picture != nil) & (picture != "")}.order{updated_at.desc}.limit(6)
-    @testimonial = Kudo.order{created_at.desc}.limit(1).first or Kudo.new(title: "Thank you!", description: "You see something nice?  Give some kudos!")
+    @testimonial = (Kudo.count > 0) ? Kudo.order{created_at.desc}.limit(1).first : Kudo.new(title: "Thank you!", description: "You see something nice?  Give some kudos!")
   end
 end
