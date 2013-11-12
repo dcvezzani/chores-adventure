99a9c436eebecf119fbc527aea5a606453f34338
David Vezzani <david_vezzani@yahoo.com>
2013-11-12 08:38:30 -0800

    puts default back; check for testimonials before using existing one

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,7 @@ class WelcomeController < ApplicationController
 
   def index
     @latest_needs = Need.where{(picture != nil) & (picture != "")}.order{updated_at.desc}.limit(6)
-    @testimonial = Kudo.new(title: "Thank you!", description: "You see something nice?  <a href='/kudos/new'>Give some kudos</a>!")
+    @testimonial = (Kudo.count > 0) ? Kudo.order{created_at.desc}.limit(1).first : Kudo.new(title: "Thank you!", description: "You see something nice?  <a href='/kudos/new'>Give some kudos</a>!")
+    
   end
 end
